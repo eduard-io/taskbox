f61f5ad8defb6e6dd0b78414cb4bd0d9d0267402
Eduard Giménez <eduard@eduard.io>
2023-07-14 11:02:50 +0200

    Enable mdx docs

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,6 +1,11 @@
 import type { StorybookConfig } from '@storybook/angular';
 const config: StorybookConfig = {
-  stories: ['../src/app/components/**/*.stories.ts'],
+  //stories: ['../src/app/*.mdx', '../src/app/components/**/*.stories.ts'],
+   stories: [
+    //👇 Your documentation written in MDX along with your stories goes here
+    '../src/**/*.mdx',
+    '../src/**/*.stories.@(js|jsx|ts|tsx)',
+  ],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
